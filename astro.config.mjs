a260af68902d38e51ce8de8edbbea044968f40be
Veloxycare <support@veloxy.care>
2025-03-17 17:50:21 +0530

    Chnage s in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import netlify from '@astrojs/netlify';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic(), tailwind()],
-  output: 'server',
+  output: 'hybrid',
   adapter: netlify(),
 });
